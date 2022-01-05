9f294a5d633bac26c936a3406145f0c3db90a248
Rudraksh Shukla <rudrakshshukla451@gmail.com>
2022-01-05 16:22:49 +0530

    user camera/Storage permisssion added change target sdk31 for android 12

--- lib/image_crop.dart
+++ lib/image_crop.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:async';
 import 'dart:io';
 import 'package:image/image.dart' as imglib;
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:share/share.dart';
+
 class ImageCropingExample extends StatefulWidget {
   final String title;
 
@@ -15,7 +18,7 @@ class ImageCropingExample extends StatefulWidget {
   _ImageCropingExampleState createState() => _ImageCropingExampleState();
 }
 
-enum AppState { free, picked,croping, cropped, split }
+enum AppState { free, picked, croping, cropped, split }
 
 class _ImageCropingExampleState extends State<ImageCropingExample> {
   AppState state;
@@ -25,6 +28,7 @@ class _ImageCropingExampleState extends State<ImageCropingExample> {
   void initState() {
     super.initState();
     state = AppState.free;
+    getStorgePermission();
   }
 
   @override
@@ -34,41 +38,44 @@ class _ImageCropingExampleState extends State<ImageCropingExample> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            state==AppState.croping?Container():Center(
-              child: state == AppState.cropped
-                  ? SizedBox(
-                      height: MediaQuery.of(context).size.height,
-                      child: imageFile != null
-                          ? Container(
-                        padding: EdgeInsets.all(10),
-                            child: GridView.count(
-                                // childAspectRatio:
-                                crossAxisCount: 3,
-                                crossAxisSpacing: 1.5,
-                                mainAxisSpacing: 1.5,
-                                children: splitImage(imageFile.readAsBytesSync()),
-                              ),
+            state == AppState.croping
+                ? Container()
+                : Center(
+                    child: state == AppState.cropped
+                        ? SizedBox(
+                            height: MediaQuery.of(context).size.height,
+                            child: imageFile != null
+                                ? Container(
+                                    padding: EdgeInsets.all(10),
+                                    child: GridView.count(
+                                      // childAspectRatio:
+                                      crossAxisCount: 3,
+                                      crossAxisSpacing: 1.5,
+                                      mainAxisSpacing: 1.5,
+                                      children: splitImage(
+                                          imageFile.readAsBytesSync()),
+                                    ),
+                                  )
+                                : Container(),
                           )
-                          : Container(),
-                    )
-                  : Center(
-                      child: imageFile != null
-                          ? Image.file(imageFile)
-                          : Container(),
-                    ),
-            ),
+                        : Center(
+                            child: imageFile != null
+                                ? Image.file(imageFile)
+                                : Container(),
+                          ),
+                  ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepOrange,
         onPressed: () {
-          if (state == AppState.free)
+          if (state == AppState.free) {
+            reqCameraPermision();
             _pickImage();
-          else if (state == AppState.picked)
+          } else if (state == AppState.picked)
             _cropImage();
           else if (state == AppState.cropped)
-
             splitImage(imageFile.readAsBytesSync());
           else if (state == AppState.split) _clearImage();
         },
@@ -89,55 +96,35 @@ class _ImageCropingExampleState extends State<ImageCropingExample> {
       return Container();
   }
 
-  ///Spliting images in 3x3
-  List<Widget> splitImage(List<int> input) {
-    imglib.Image image = imglib.decodeImage(imageFile.readAsBytesSync());
-    int x = 1, y = 0;
-    int width = (image.width / 3).round();
-    int height = (image.height / 3).round();
-
-    List<imglib.Image> parts = <imglib.Image>[];
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        parts.add(imglib.copyCrop(image, x, y, width, height));
-        x += width;
-      }
-      x = 0;
-      y += height;
-    }
-    List<Widget> output = <Widget>[];
-    for (var img in parts) {
-      output.add(GestureDetector(
-          onTap: () {
-            shareImage();
-            savingFile(img,);
-          },
-          child: Image.memory(imglib.encodeJpg(img))));
-    }
-    return output;
-  }
-
-  File filepath;
-///Saving file locally
-  Future<void> savingFile(imglib.Image img) async {
-    var imagePath = imglib.encodeJpg(img);
-    final external = (await getExternalStorageDirectory()).path + '/insta.png';
-    try {
-      final file = File(external);
-      filepath = await file.writeAsBytes(imagePath);
-    } catch(error) {
-      print('///// ERROR: $error');
+  ///Storage Permission
+  Future<void> getStorgePermission() async {
+    final serviceStatus = await Permission.storage.isGranted;
+    bool isStorage = serviceStatus == ServiceStatus.enabled;
+    final storageStatus = await Permission.storage.request();
+    if (storageStatus == PermissionStatus.granted) {
+      print("Storage permission granted");
+    } else if (storageStatus == PermissionStatus.denied) {
+      print("Storage permission denied");
+    } else if (storageStatus == PermissionStatus.permanentlyDenied) {
+      showToast("Storage Permission is required");
+      print("Storage permission permanently denied");
     }
   }
 
-  ///Converting image to file
-
-  ///Share image
-  Future<void> shareImage() async {
-    final RenderBox box = context.findRenderObject();
-    Share.shareFiles([filepath.path],
-        subject: "this is subject",
-        sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+  /// Camera Permission
+  Future<void> reqCameraPermision() async {
+    final serviceStatus = await Permission.camera.isGranted;
+    bool isCameraOn = serviceStatus == ServiceStatus.enabled;
+    final status = await Permission.camera.request();
+    if (status == PermissionStatus.granted) {
+      print("permission granted");
+    } else if (status == PermissionStatus.denied) {
+      print("permission denied");
+    } else if (status == PermissionStatus.permanentlyDenied) {
+      showToast("Camera Permission is required");
+      print('permanent denied');
+      await openAppSettings();
+    }
   }
 
   /// pick image from camera
@@ -187,11 +174,75 @@ class _ImageCropingExampleState extends State<ImageCropingExample> {
     }
   }
 
+  ///Splitting images in 3x3
+  List<Widget> splitImage(List<int> input) {
+    imglib.Image image = imglib.decodeImage(imageFile.readAsBytesSync());
+    int x = 1, y = 0;
+    int width = (image.width / 3).round();
+    int height = (image.height / 3).round();
+
+    List<imglib.Image> parts = <imglib.Image>[];
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        parts.add(imglib.copyCrop(image, x, y, width, height));
+        x += width;
+      }
+      x = 0;
+      y += height;
+    }
+    List<Widget> output = <Widget>[];
+    for (var img in parts) {
+      output.add(GestureDetector(
+          onTap: () {
+            shareImage();
+            savingFile(
+              img,
+            );
+          },
+          child: Image.memory(imglib.encodeJpg(img))));
+    }
+    return output;
+  }
+
+  File filepath;
+
+  ///Saving file locally
+  Future<void> savingFile(imglib.Image img) async {
+    var imagePath = imglib.encodeJpg(img);
+    final external = (await getApplicationDocumentsDirectory()).path + '/${TimeOfDay.now().toString()}.png';
+    try {
+      final file = File(external);
+      filepath = await file.writeAsBytes(imagePath);
+    } catch (error) {
+      print('///// ERROR: $error');
+    }
+  }
+
+  ///Share image
+  Future<void> shareImage() async {
+    final RenderBox box = context.findRenderObject();
+    Share.shareFiles([filepath.path],
+        subject: "this is subject",
+        sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+  }
+
   ///clear image
   void _clearImage() {
     imageFile = null;
     setState(() {
       state = AppState.free;
     });
+  }
+
+  /// Flutter Toast
+  void showToast(String msg) {
+    Fluttertoast.showToast(
+      msg: msg,
+      fontSize: 16.0,
+      backgroundColor: Colors.transparent,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      textColor: Colors.white,
+    );
   }
 }
